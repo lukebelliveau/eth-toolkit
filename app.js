c2a78b8a912135d03822cefd5968430135632a52
lukebelliveau <luke.belliveau@gmail.com>
2018-01-14 11:13:44 -0500

    Add switch block for commands

--- app.js
+++ app.js
@@ -3,56 +3,63 @@ const Web3 = require('web3');
 const solc = require("solc");
 const fs = require('fs');
 
-const myAccount = process.argv[3];
+const deploy = () => {
+    const myAccount = process.argv[4];
 
-// Start Parity locally and connect via RPC
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    // Start Parity locally and connect via RPC
+    const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    
+    if(process.argv.length < 3) {
+        console.log("Please enter .sol file as first argument.");
+        process.exit();
+    };
+    
+    const source = fs.readFileSync(process.argv[3]).toString();
+    
+    const contracts = solc.compile(source).contracts
+    const compiledContract = contracts[':greeter'];
+    
+    // grab artifacts
+    const abi = JSON.parse(compiledContract.metadata).output.abi;
+    const bytecode = '0x' + compiledContract.bytecode;
+    
+    //TODO: sign this contract without Parity UI
+    const contract = new web3.eth.Contract(abi, "", {
+        from: myAccount,
+        data: bytecode
+    });
+    
+    // Initialize with constructor arguments and send to blockchain
+    const myMessage = "bonsoir";
+    console.log("Attempting to deploy contract...");
+    contract.deploy({
+        arguments: [myMessage]
+    })
+    .send()
+    .then(res => {
+        console.log("SUCCESS!")
+        const deployedAddress = res._address;
+        const deployedContract = new web3.eth.Contract(abi, deployedAddress);
+    
+        console.log("Contract deployed to address:");
+        console.log("- " + deployedAddress)
+    
+        console.log("Calling say() on deployed contract...");
+        return deployedContract.methods.say().call({from: myAccount})
+    })
+    .then(res => {
+        console.log("Called say()! Response:")
+        console.log("- " + res)
+    
+        // expect(res === myMessage)
+    })
+    .catch(err => {
+        console.log("ERROR!")
+        console.log("- " + err)
+    })
+}
 
-if(process.argv.length < 3) {
-    console.log("Please enter .sol file as first argument.");
-    process.exit();
-};
-
-const source = fs.readFileSync(process.argv[2]).toString();
-
-const contracts = solc.compile(source).contracts
-const compiledContract = contracts[':greeter'];
-
-// grab artifacts
-const abi = JSON.parse(compiledContract.metadata).output.abi;
-const bytecode = '0x' + compiledContract.bytecode;
-
-//TODO: sign this contract without Parity UI
-const contract = new web3.eth.Contract(abi, "", {
-    from: myAccount,
-    data: bytecode
-});
-
-// Initialize with constructor arguments and send to blockchain
-const myMessage = "bonsoir";
-console.log("Attempting to deploy contract...");
-contract.deploy({
-    arguments: [myMessage]
-})
-.send()
-.then(res => {
-    console.log("SUCCESS!")
-    const deployedAddress = res._address;
-    const deployedContract = new web3.eth.Contract(abi, deployedAddress);
-
-    console.log("Contract deployed to address:");
-    console.log("- " + deployedAddress)
-
-    console.log("Calling say() on deployed contract...");
-    return deployedContract.methods.say().call({from: myAccount})
-})
-.then(res => {
-    console.log("Called say()! Response:")
-    console.log("- " + res)
-
-    // expect(res === myMessage)
-})
-.catch(err => {
-    console.log("ERROR!")
-    console.log("- " + err)
-})
+switch (process.argv[2]) {
+    case "deploy": deploy();
+    default: console.log("Invalid command.");
+}
